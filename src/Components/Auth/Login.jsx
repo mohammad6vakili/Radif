e8bc99c5e735103865be361608d60c393f0b6ba7
mohammad6vakili <59134745+mohammad6vakili@users.noreply.github.com>
2021-12-08 17:55:41 +0330

    complete login

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import "./Login.css";
+import { useHistory } from 'react-router';
+import ReactCodeInput from "react-code-input";
 import vectorOne from "../../Assets/Images/login-vector-one.svg";
 import vectorTwo from "../../Assets/Images/login-vector-two.svg";
+import penImage from "../../Assets/Images/pen-green.svg";
 import { Button, Input } from 'antd';
+import Colors from "../../Helper/Colors";
+import FormatHelper from "../../Helper/FormatHelper";
+import Countdown from "react-countdown";
 import { toast } from 'react-toastify';
 
 
 
 const Login =()=>{
-
+    const history=useHistory();
     const [step , setStep]=useState(0);
     const [mobile , setMobile]=useState("");
     const [nationalNumber , setNationalNumber]=useState("");
+    const [isCount , setIsCount]=useState(false);
+
 
     const getCode=()=>{
         if(mobile===""){
@@ -34,8 +42,24 @@ const Login =()=>{
             toast.success("کد تایید با موفقیت برای شما ارسال شد",{
                 position: toast.POSITION.BOTTOM_LEFT
             });
+            setStep(1);
+            setIsCount(true);
         }
     }
+
+
+    const sendCode=(e)=>{
+        if(e.length===5){
+            history.push("/home");
+            toast.success("خوش آمدید",{
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+        }
+    }
+
+    const renderer = ({ minutes, seconds}) => {
+        return <span>{minutes}:{seconds}</span>;
+    };
 
     return(
         <div className="login page-wrap">
@@ -66,6 +90,51 @@ const Login =()=>{
                             className="green-btn submit-btn"
                         >
                             دریافت رمز یک بار مصرف
+                        </Button>
+                    </div>
+                </>
+            }
+            {step===1 &&
+                <>
+                    <img src={vectorTwo} alt="login" />
+                    {isCount ?
+                        <>
+                            <span 
+                                style={{marginTop:"32vh",width:"150px",fontSize:"16px",textAlign:"center",zIndex:"999",color:Colors.black}}
+                            >
+                                کد ۵ رقمی به شماره همراه {FormatHelper.toPersianString(mobile)} ارسال شد
+                            </span>
+                            <div className="timer">
+                                <Countdown 
+                                    date={Date.now() + 120000}
+                                    autoStart={true} 
+                                    zeroPadTime={2}
+                                    renderer={renderer}
+                                    onComplete={()=>setIsCount(false)}
+                                />
+                            </div>
+                        </>
+                        :
+                        <>
+                            <span style={{zIndex:"9999",marginTop:"22vh"}}>کد تایید ارسال نشد؟</span>
+                            <span onClick={getCode} style={{color:Colors.green,zIndex:"9999",cursor:"pointer"}}>ارسال مجدد کد تایید</span>
+                        </>
+                    }
+                    <ReactCodeInput
+                        type='number'
+                        fields={5} 
+                        onChange={(e)=>sendCode(e)}
+                    />
+                    <div onClick={()=>setStep(0)} className="correct-number">
+                        <img src={penImage} alt="edit" />
+                        <span style={{color:Colors.green,marginRight:"5px",paddingTop:"2px"}}>اصلاح شماره همراه</span>
+                    </div>
+                    <div className="bottom-btn-box">
+                        <Button 
+                            onClick={sendCode}
+                            className="green-btn submit-btn"
+                        >
+                            ورود به حساب کاربری 
                         </Button>
                     </div>
                 </>
